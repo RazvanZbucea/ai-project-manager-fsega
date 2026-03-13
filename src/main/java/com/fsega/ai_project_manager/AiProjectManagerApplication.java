be00c9af4a9b761bb9244191559d814b7ab52ffa
RazvanZbucea <razvan_zb@yahoo.ro>
2026-03-13 22:23:17 +0200

    first commit

---
+++ src/main/java/com/fsega/ai_project_manager/AiProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fsega.ai_project_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsega/ai_project_manager/AiProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsega.ai_project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
