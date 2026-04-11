ff1483c72744be09520829bcfe1498449a137536
RazvanZbucea <razvan_zb@yahoo.ro>
2026-04-11 14:43:06 +0300

    added JwtAuthenticationFilter

--- src/main/java/com/fsega/ai_project_manager/service/JwtService.java
+++ src/main/java/com/fsega/ai_project_manager/service/JwtService.java
@@ -1,10 +1,10 @@
 package com.fsega.ai_project_manager.service;
 
-import com.fsega.ai_project_manager.model.CustomUserDetails;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -18,25 +18,24 @@ public class JwtService {
     @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
 
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().subject(userDetails.getUsername()).issuedAt(new Date(System.currentTimeMillis())).expiration(new Date(System.currentTimeMillis() + jwtExpiration)).signWith(getSignInKey()).compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
     private SecretKey getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    private String generateToken(CustomUserDetails user) {
-        return Jwts.builder().subject(user.getUsername()).issuedAt(new Date(System.currentTimeMillis())).expiration(new Date(System.currentTimeMillis() + jwtExpiration)).signWith(getSignInKey()).compact();
-    }
-
-    private String extractUsername(String token) {
-        return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload().getSubject();
-    }
-
-    private boolean isTokenValid(String token, CustomUserDetails user) {
-        return user.getUsername().equals(extractUsername(token)) && isTokenExpired(token);
-    }
-
     private boolean isTokenExpired(String token) {
         return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
-
 }
