58c1ed87f9ac27e60cb61f983cbd38cd58769d30
RazvanZbucea <razvan_zb@yahoo.ro>
2026-04-22 16:08:18 +0300

    added login component

--- ai-project-manager-frontend/src/app/features/auth/login/login.component.ts
+++ ai-project-manager-frontend/src/app/features/auth/login/login.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../../core/services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
+  selector: 'login-component',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  })
+
+  constructor(private authService: AuthService) {
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid) {
+
+      const loginRequest: LoginRequest = {
+        username: this.loginForm.value.username
+      }
+
+    }
+  }
+}
--- ai-project-manager-frontend/src/app/shared/models/auth-user-create.ts
+++ ai-project-manager-frontend/src/app/shared/models/auth-user-create.ts
@@ -1,4 +1,4 @@
-interface AdminUserCreate {
+interface AuthUserCreate {
   username: string,
   email: string
   password: string,
