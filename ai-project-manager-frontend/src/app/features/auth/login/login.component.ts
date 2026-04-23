0265b9bade8ccb1ef5d8d84c4beb5a6eeb3a9e3e
RazvanZbucea <razvan_zb@yahoo.ro>
2026-04-23 11:29:38 +0300

    redo auth.service.ts

--- ai-project-manager-frontend/src/app/features/auth/login/login.component.ts
+++ ai-project-manager-frontend/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../../core/services/auth.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'login-component',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  imports: [ReactiveFormsModule]
+  styleUrls: ['./login.component.scss'],
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class LoginComponent {
   loginForm = new FormGroup({
@@ -17,13 +18,15 @@ export class LoginComponent {
   constructor(private authService: AuthService) {
   }
 
-  onSubmit(){
-    if(this.loginForm.valid) {
+  onSubmit() {
+    if (this.loginForm.valid) {
 
       const loginRequest: LoginRequest = {
-        username: this.loginForm.value.username
-      }
+        username: this.loginForm.value.username ?? '',
+        password: this.loginForm.value.password ?? ''
+      };
 
+      console.log('Datele pregătite pentru backend:', loginRequest);
     }
   }
 }
